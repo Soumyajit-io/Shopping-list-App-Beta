7ffa9cc9668f71ebf20091f6506bd8364b1f614b
Soumyajit-io <soumyajit010805@gmail.com>
2025-07-16 18:40:42 +0530

    Initial Version

---
+++ app/src/main/java/com/soumyajit/spa/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.soumyajit.spa
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.soumyajit.spa.ui.theme.SpaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SpaTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center)
+                    {
+                        Button(onClick = {},
+                            modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                            Text("Add Iteam")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
